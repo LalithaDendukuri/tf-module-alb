ed7e9ad8bfb32ad73f3e66f28b8966f29af58cb8
Lalitha <lalitha.d339@gmail.com>
2023-09-01 16:52:29 +0530

    terraform variables

---
+++ vars.tf
@@ -0,0 +1,8 @@
+variable "env" {}
+variable "tags" {}
+variable "internal" {}
+variable "lb_type" {}
+variable "sg_ingress_cidr" {}
+variable "vpc_id" {}
+variable "sg_port" {}
+variable "subnets" {}
